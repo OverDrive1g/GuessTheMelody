a4570c203fdcd088dedc596720e3c0432e1f895b
OverDrive <v.overdrive@gmail.com>
2016-11-04 16:06:01 +0300

    Added all the things

--- WindowsFormsApplication1/Properties/Settings.Designer.cs
+++ WindowsFormsApplication1/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GuessTheMelody.Properties
+namespace WindowsFormsApplication1.Properties
 {
 
 
